ef5dd7833e7d12a32a889b9b08fa1b85dfebde7a
Dmitrii <aleksdima@mail.ru>
2023-02-15 22:26:47 +0300

    Completed task 8

--- Homework2/Task10.py
+++ Homework2/Task10.py
@@ -1,6 +1,6 @@
 from random import randint
-coin_count=int(input("Введите количество монеток:"))
-a= [randint(0,1) for i in range(coin_count)]
+coin_count = int(input("Введите количество монеток:"))
+a = [randint(0, 1) for i in range(coin_count)]
 print(a)
 count1 = 0 
 count2 = 0
@@ -9,7 +9,11 @@ while i < len(a):
     if a[i] == 0:
         count1 += 1
     if a[i] == 1:
-        count2 +=1
+        count2 += 1
     i += 1
-print(f"Количество 0:{count1}")
-print(count2)
+print(f"Количество орлов:{count1}")
+print(f"Количество режек:{count2}")
+if count1 > count2:
+    print(f"Необходимо перевернуть {count2} монеток")
+else:
+    print(f"Необходимо перевернуть {count1} монеток")
